2748d5d94c27bbb000d57595249f1722732b26d3
vaibha987 <v2goyal95@gmail.com>
2024-09-29 21:17:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-//your JS code here. If required.
-let promise=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-			resolve([1,2,3,4])
-	},3000)
-})
-promise.then((numbers)=>{
-return numbers.filter((item)=>item%2==0)
-}).then((numbers)=>{
-return numbers.map((num)=>num*2)
-}).then((numbers) => {
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve([1, 2, 3, 4]);
+  }, 3000);
+});
+
+promise
+  .then((numbers) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(numbers.filter((item) => item % 2 == 0));
+      }, 1000);
+    });
+  })
+  .then((numbers) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(numbers.map((num) => num * 2));
+      }, 2000);
+    });
+  })
+  .then((numbers) => {
     document.getElementById('output').textContent = numbers.toString();
   });
