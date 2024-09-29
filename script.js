a77a6a10908286caa3df3a45f572040ec5c31488
vaibha987 <v2goyal95@gmail.com>
2024-09-29 20:26:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let promise=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+			resolve([1,2,3,4])
+	},3000)
+})
+promise.then((numbers)=>{
+return number.filter((item)=>item%2==0)
+}).then((numbers)=>{
+return number.map((num)=>num*2)
+}).then((numbers) => {
+    document.getElementById('output').textContent = numbers.toString();
+  });
